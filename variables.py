daa9a759edd49f8f2795cb1011f069b25491d284
Daniel <danydge5@gmail.com>
2023-06-09 16:21:56 -0600

    02 Variables

---
+++ variables.py
@@ -0,0 +1,14 @@
+#Python es No tipado
+name="Daniel"
+last_name="Degante"
+age=19
+height=1.7
+is_student=True
+groups=["3A","3B","3C"]
+
+# FUNCTION TYPE 
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+print(type(groups))
